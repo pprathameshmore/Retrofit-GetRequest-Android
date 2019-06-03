2e2ab065cd2539754e0867cca6d9f3403db669eb
Prathamesh More <moreprathameshmore@hotmail.com>
2019-06-03 10:45:47 +0530

    Initial commit

---
+++ app/src/main/java/com/prathameshmore/retrofit_android/JsonPlaceHolderApi.java
@@ -0,0 +1,13 @@
+package com.prathameshmore.retrofit_android;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface JsonPlaceHolderApi {
+
+    @GET("posts")
+    Call<List<Post>> getPosts();
+
+}
